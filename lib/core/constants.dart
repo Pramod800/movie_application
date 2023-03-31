f5509991a936dd6dc2a627a1a75cb918a1d58074
Pramod800 <timilsinapramod8888@gmail.com>
2023-03-31 16:38:22 +0545

    constants added like popular , top rated, trending

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -5,4 +5,14 @@ class MovieConstants {
   static String key = 'caebc202bd0a26f84f4e0d986beb15cd';
   static String upcomingMovies =
       "http://api.themoviedb.org/3/movie/upcoming?api_key=caebc202bd0a26f84f4e0d986beb15cd";
+  static String popularMovies =
+      "https://api.themoviedb.org/3/movie/popular?api_key=$key&language=en-US&page=1";
+
+  static String topRated =
+      "https://api.themoviedb.org/3/movie/top_rated?api_key=$key&language=en-US&page=1";
+
+  static String trendingMovies =
+      "http://api.themoviedb.org/3/movie/upcoming?api_key=$key";
 }
+// Latedt Movies::::   https://api.themoviedb.org/3/movie/latest?api_key=<<api_key>>&language=en-US
+// similar movies :::   https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=<<api_key>>&language=en-US&page=1
