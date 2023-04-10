9af3d11c4671bb79b3a73359dde40c4f1daa1be1
Pramod800 <timilsinapramod8888@gmail.com>
2023-04-10 22:51:01 +0545

    blocs created for registration

--- lib/features/movies/data/models/movie_models.g.dart
+++ lib/features/movies/data/models/movie_models.g.dart
@@ -8,7 +8,7 @@ part of 'movie_models.dart';
 
 class MovieCardAdapter extends TypeAdapter<MovieCardModel> {
   @override
-  final int typeId = 0;
+  final int typeId = 1;
 
   @override
   MovieCardModel read(BinaryReader reader) {
